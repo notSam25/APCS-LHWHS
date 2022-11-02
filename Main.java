7d6f4a924b8b387fd860eb556cd0180f28c22441
notSam <ghvgamingproject@gmail.com>
2022-11-02 15:29:01 +0000

    add solution

---
+++ Main.java
@@ -0,0 +1,8 @@
+import ConsumerLab.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Review rv = new Review();
+        System.out.println(rv.getTotalValue("words.txt"));
+    }
+}
